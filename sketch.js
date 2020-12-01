fb2988c271e08e492b165fc1c8f3cd9e5aec15bd
shauryakedia237 <69040024+shauryakedia237@users.noreply.github.com>
2020-12-01 21:28:01 +0530

    jn gb
    b.   hjmn

--- sketch.js
+++ sketch.js
@@ -88,20 +88,20 @@ function keyPressed() {
   }
 
   function detectollision(lstone,lmango){
-	/*var collision = Matter.SAT.collides(lstone,lmango);
+	var collision = Matter.SAT.collides(lstone,lmango);
 	if(collision.collided){
 		console.log("collided");
 		Matter.Body.setStatic(lmango,false);	
-	}*/
+	}
   mangoBodyPosition=lmango.body.position
   stoneBodyPosition=lstone.body.position
   
   var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
-  //console.log(distance)
- // console.log(lmango.r+lstone.r)
+  console.log(distance)
+  console.log(lmango.r+lstone.r)
   	if(distance<=lmango.r+lstone.r)
     {
-      //console.log(distance);
+      console.log(distance);
   	  Matter.Body.setStatic(lmango.body,false);
     }
 
